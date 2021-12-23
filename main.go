0f93d74c05a4a23ae5d4ecb50cdfaa0cd5ceb965
lijunyu <lijunyu@ljygogogo.com>
2021-12-23 23:12:10 +0800

    grpc server

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"google.golang.org/grpc"
+	"net"
 	"recommend/config"
+	"recommend/idl/gen/recommend"
 	"recommend/model"
 	"recommend/service"
 )
@@ -13,6 +18,21 @@ func main() {
 	if err := model.InitModel(); err != nil {
 		panic(err)
 	}
-
 	service.InitService()
+
+	var port int
+	var ip string
+	flag.IntVar(&port, "port", 50000, "port for the service")
+	flag.StringVar(&ip, "ip", "127.0.0.1", "ip for the service")
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", ip, port))
+	if err != nil {
+		panic(err)
+	}
+	grpcServer := grpc.NewServer()
+	recommend.RegisterRecommenderServer(grpcServer, &RecommendServer{})
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
 }
